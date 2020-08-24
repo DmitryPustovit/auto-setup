02213f23acdd3904735747450f77d15d2933d8c8
Dmitry Pustovit <Dmitryp222@gmail.com>
2020-08-23 20:29:39 -0700

    Need to fix hostname

--- Raspberry Pi OS/setup.sh
+++ Raspberry Pi OS/setup.sh
@@ -19,7 +19,7 @@ fi
 # Technically this isn't needed because, this script should be cloned via git but, eh why not...
 sudo apt install git
 
-hostnamectl set-hostname $1
+### TODO fix hostnamectl set-hostname $1
 ### TODO fix password change command passwd pi $2
 
 
