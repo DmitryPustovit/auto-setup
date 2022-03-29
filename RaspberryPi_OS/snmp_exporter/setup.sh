b8260dcb12441afb52a7e868e5c6cae64c55ac77
Dmitry Pustovit <Dmitryp222@gmail.com>
2022-03-28 23:11:55 -0700

    Updates

--- RaspberryPi_OS/snmp_exporter/setup.sh
+++ RaspberryPi_OS/snmp_exporter/setup.sh
@@ -17,3 +17,15 @@ sudo chmod +x /usr/local/bin/snmp_exporter
 sudo useradd -m -s /bin/bash snmp_exporter
 sudo mkdir /var/lib/snmp_exporter
 sudo chown -R snmp_exporter:snmp_exporter /var/lib/snmp_exporter
+
+# Reload systemd
+wget https://raw.githubusercontent.com/prometheus/snmp_exporter/main/examples/systemd/snmp_exporter.service
+sudo cp snmp_exporter.service /etc/systemd/system/snmp_exporter.service
+
+# Reload systemd
+sudo systemctl daemon-reload 
+sudo systemctl enable snmp_exporter.service
+sudo systemctl start snmp_exporter.service
+
+# Sanity check 
+systemctl status snmp_exporter
