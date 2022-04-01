8b8537a6354fedc60d079c9c019a303847b78f33
Dmitry Pustovit <Dmitryp222@gmail.com>
2022-04-01 00:35:03 -0700

    Edge Metrics

--- Edge X Router/export_leases.sh
+++ Edge X Router/export_leases.sh
@@ -6,15 +6,21 @@ get_leases() {
         $RUNDIR show dhcp leases | tail -n +3
 }
 
+RESULTS=''
+
 while IFS= read -r Lease
 do
         array=($(echo "$Lease" | tr ',' '\t'))
 
-        printf "dhcp_lease{\
-ip_address="${array[0]}", \
-hardware_address="${array[1]}", \
-pool="${array[4]}", \
-client_name="${array[5]}" \
-} $(date -d "${array[2]////-} ${array[3]}" +"%s")\n"
+        RESULTS+=$( printf "dhcp_lease{\
+ip_address=\""${array[0]}\"", \
+hardware_address=\""${array[1]}\"", \
+pool=\""${array[4]}\"", \
+client_name=\""${array[5]}\"" \
+} $(date -d "${array[2]////-} ${array[3]}" +"%s")" )
+
+        RESULTS+="\n"
 
 done < <(printf '%s\n' "$(get_leases)")
+
+echo -e "$RESULTS" | curl -v -H "Connection: close" --data-binary @- http://<PUSHGATEWAY>:9091/metrics/job/dhcp_exporter/instance/<EDGE_COUTER>
