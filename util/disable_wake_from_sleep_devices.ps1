54d7efbede8ba6126770c950974c1568fdfdbef7
Dmitry Pustovit <Dmitryp222@gmail.com>
2024-11-28 00:10:19 -0800

    Create disable_wake_from_sleep_devices.ps1

---
+++ util/disable_wake_from_sleep_devices.ps1
@@ -0,0 +1,22 @@
+$devices = powercfg -devicequery wake_armed
+
+if ($devices -eq "NONE") {
+    Write-Host "No devices found."
+    exit
+}
+
+foreach ($device in $devices) {
+    if ([string]::IsNullOrWhiteSpace($device)) {
+        continue
+    }
+
+    $confirmMessage = "Disable ability to wake from sleep for device: [$device] (Y/N)"
+    $response = Read-Host -Prompt $confirmMessage
+
+    if ($response -eq "Y" -or $response -eq "y") {
+        Write-Host "Disabling device: [$deviceID]"
+        powercfg -devicedisablewake $deviceID
+    }
+}
+
+Write-Host "Done."
